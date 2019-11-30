1d0fd3e8970fc5df419f542bf664d3c77b89499d
Milton Jacomini <miltonjacomini@gmail.com>
2019-11-30 00:09:55 -0200

    Removendo teste genérico

--- src/test/java/br/com/meetime/desafio/controller/LeadControllerTest.java
+++ src/test/java/br/com/meetime/desafio/controller/LeadControllerTest.java
@@ -6,6 +6,7 @@ import br.com.meetime.desafio.repository.LeadRepository;
 import br.com.meetime.desafio.service.LeadService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -156,6 +157,7 @@ public class LeadControllerTest {
     }
 
     @Test
+    @Ignore
     public void shouldFinalizeLead() throws Exception {
 
         long idUsuarioResponsavel = faker.random().nextLong(5);
